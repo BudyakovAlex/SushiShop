e24e97601b9d2cbe6c739a557b55da2fe6c4384a
Vladislav Khasenevich <bender9642459@gmail.com>
2020-10-30 23:54:28 +0300

    add related products

--- SushiShop.Ios/Views/ViewControllers/Products/ProductDetailsViewController.cs
+++ SushiShop.Ios/Views/ViewControllers/Products/ProductDetailsViewController.cs
@@ -1,9 +1,13 @@
 ﻿using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.ViewControllers;
 using MvvmCross.Platforms.Ios.Binding;
 using MvvmCross.Platforms.Ios.Presenters.Attributes;
+using SushiShop.Core.ViewModels.Menu.Items;
 using SushiShop.Core.ViewModels.Products;
 using SushiShop.Ios.Common.Styles;
 using SushiShop.Ios.Converters;
+using SushiShop.Ios.Delegates;
+using SushiShop.Ios.Sources;
+using SushiShop.Ios.Views.Cells.Menu;
 using UIKit;
 
 namespace SushiShop.Ios.Views.ViewControllers.Products
@@ -11,6 +15,8 @@ namespace SushiShop.Ios.Views.ViewControllers.Products
     [MvxChildPresentation(Animated = true)]
     public partial class ProductDetailsViewController : BaseViewController<ProductDetailsViewModel>
     {
+        private CollectionViewSource source;
+
         protected override void InitStylesAndContent()
         {
             base.InitStylesAndContent();
@@ -36,13 +42,21 @@ namespace SushiShop.Ios.Views.ViewControllers.Products
             bindingSet.Bind(BackButton).For(v => v.BindTap()).To(vm => vm.CloseCommand);
             bindingSet.Bind(OldPriceLabel).For(v => v.AttributedText).To(vm => vm.OldPrice)
                 .WithConversion<StringToStrikethroughAttributedTextConverter>();
-            // bindingSet.Bind(StepperView)
+            bindingSet.Bind(StepperView).For(v => v.ViewModel).To(vm => vm.StepperViewModel);
+            bindingSet.Bind(source).For(v => v.ItemsSource).To(vm => vm.Items);
 
             bindingSet.Apply();
         }
 
         private void InitCollectionView()
         {
+            source = new CollectionViewSource(BuyAnotherCollectionView)
+                .Register<ProductItemViewModel>(ProductItemViewCell.Nib, ProductItemViewCell.Key);
+
+            BuyAnotherCollectionView.Source = source;
+            BuyAnotherCollectionView.Delegate = new RelatedProductsCollectionViewDelegateFlow();
+            BuyAnotherCollectionView.CollectionViewLayout = new UICollectionViewFlowLayout();
+            (BuyAnotherCollectionView.CollectionViewLayout as UICollectionViewFlowLayout).ScrollDirection = UICollectionViewScrollDirection.Horizontal;
         }
 
         public override void ViewWillAppear(bool animated)
