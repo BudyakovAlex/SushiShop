17145678d7f0094ebf4ee4d1bca221ea8d60690b
Vadym Rybalko <vadim_lost@outlook.com>
2020-11-13 01:08:05 +0200

    added simple fileds

--- SushiShop.Core/ViewModels/Cart/CartViewModel.cs
+++ SushiShop.Core/ViewModels/Cart/CartViewModel.cs
@@ -1,8 +1,30 @@
-﻿using BuildApps.Core.Mobile.MvvmCross.ViewModels.Abstract;
+﻿using System.Threading.Tasks;
+using BuildApps.Core.Mobile.MvvmCross.Commands;
+using BuildApps.Core.Mobile.MvvmCross.ViewModels.Abstract;
+using MvvmCross.Commands;
 
 namespace SushiShop.Core.ViewModels.Cart
 {
     public class CartViewModel : BasePageViewModel
     {
+        public CartViewModel()
+        {
+            CheckoutCommand = new SafeAsyncCommand(ExecutionStateWrapper, CheckoutAsync);
+        }
+
+        //public IMvxCommand AddSouceCommand { get; }
+        //public IMvxCommand AddBagCommand { get; }
+        public IMvxCommand CheckoutCommand { get; }
+
+        public string Title => string.Empty;
+        public string ProductUrl => string.Empty;
+        public string ProductName => string.Empty;
+        public string TotalPrice => string.Empty;
+
+        private Task CheckoutAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 }
