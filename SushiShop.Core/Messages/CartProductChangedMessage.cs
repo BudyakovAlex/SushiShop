0b401ea8dbab60dd83e8250ad5d876bf6f2eaa99
Alex Budiakov <>
2020-12-01 21:14:23 +0200

    Fixed price issues

--- SushiShop.Core/Messages/CartProductChangedMessage.cs
+++ SushiShop.Core/Messages/CartProductChangedMessage.cs
@@ -1,11 +1,22 @@
 ﻿using MvvmCross.Plugin.Messenger;
+using SushiShop.Core.Data.Enums;
+using SushiShop.Core.Data.Models.Cart;
 
 namespace SushiShop.Core.Messages
 {
     public class CartProductChangedMessage : MvxMessage
     {
-        public CartProductChangedMessage(object sender) : base(sender)
+        public CartProductChangedMessage(
+            object sender,
+            ProductChangeAction productChangeAction,
+            CartProduct cartProduct) : base(sender)
         {
+            ProductChangeAction = productChangeAction;
+            CartProduct = cartProduct;
         }
+
+        public ProductChangeAction ProductChangeAction { get; }
+
+        public CartProduct CartProduct { get; }
     }
 }
