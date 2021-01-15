84890718f92c9a4a0d81178f765daa935c1db119
Vladislav Khasenevich <bender9642459@gmail.com>
2021-01-15 09:18:53 +0000

    Merged in fix/ui-bugs (pull request #43)
    Add delegate for open phone in WKWebView and fix ShopDetailsBottom
    
    * add delegate for open phone in WKWebView and fix ShopDetailsBottom
    
    Approved-by: Олександр Будяков

---
+++ SushiShop.Ios/Delegates/WKWebViewNavigationDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using WebKit;
+
+namespace SushiShop.Ios.Delegates
+{
+    public class WKWebViewNavigationDelegate : NSObject, IWKNavigationDelegate
+    {
+        [Export("webView:decidePolicyForNavigationAction:decisionHandler:")]
+        public void DecidePolicy(WKWebView webView, WKNavigationAction navigationAction, Action<WKNavigationActionPolicy> decisionHandler)
+        {
+            if (navigationAction.Request.Url?.Scheme == "tel")
+            {
+                UIApplication.SharedApplication.OpenUrl(navigationAction.Request.Url);
+                decisionHandler.Invoke(WKNavigationActionPolicy.Cancel);
+            }
+            else
+            {
+                decisionHandler.Invoke(WKNavigationActionPolicy.Allow);
+            }
+        }
+    }
+}
