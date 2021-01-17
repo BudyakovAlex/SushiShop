a538cf8ee365ea17fb3e1bfacab79e1d6cc30852
Alex Budiakov <>
2021-01-17 23:10:53 +0200

    wip

--- SushiShop.Core/ViewModels/Orders/Items/DeliveryZoneItemViewModel.cs
+++ SushiShop.Core/ViewModels/Orders/Items/DeliveryZoneItemViewModel.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using SushiShop.Core.Data.Models.Shops;
+
 namespace SushiShop.Core.ViewModels.Orders.Items
 {
     public class DeliveryZoneItemViewModel
     {
-        public DeliveryZoneItemViewModel()
+        public DeliveryZoneItemViewModel(DeliveryZone deliveryZone)
         {
+            DeliveryZone = deliveryZone;
         }
+
+        public DeliveryZone DeliveryZone { get; }
     }
 }
