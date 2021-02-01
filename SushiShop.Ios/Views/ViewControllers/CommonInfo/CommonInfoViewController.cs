57c7ee98f0e34a6e0a36274dd8a919c03bc2bbe8
Alex Budiakov <>
2021-02-01 13:48:56 +0200

    fixed redirect

--- SushiShop.Ios/Views/ViewControllers/CommonInfo/CommonInfoViewController.cs
+++ SushiShop.Ios/Views/ViewControllers/CommonInfo/CommonInfoViewController.cs
@@ -1,14 +1,35 @@
 ﻿using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.ViewControllers;
+using Foundation;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.Platforms.Ios.Presenters.Attributes;
 using SushiShop.Core.ViewModels.Common;
 using SushiShop.Ios.Extensions;
+using System;
+using UIKit;
+using WebKit;
 
 namespace SushiShop.Ios.Views.ViewControllers.CommonInfo
 {
     [MvxChildPresentation]
-    public partial class CommonInfoViewController : BaseViewController<CommonInfoViewModel>
+    public partial class CommonInfoViewController : BaseViewController<CommonInfoViewModel>, IWKNavigationDelegate
     {
+        [Export("webView:decidePolicyForNavigationAction:decisionHandler:")]
+        public void DecidePolicy(
+            WKWebView webView,
+            WKNavigationAction navigationAction,
+            Action<WKNavigationActionPolicy> decisionHandler)
+        {
+            UIApplication.SharedApplication.OpenUrl(navigationAction.Request.Url);
+            decisionHandler.Invoke(WKNavigationActionPolicy.Allow);
+        }
+
+        protected override void InitStylesAndContent()
+        {
+            base.InitStylesAndContent();
+
+            WebView.NavigationDelegate = this;
+        }
+
         protected override void Bind()
         {
             base.Bind();
