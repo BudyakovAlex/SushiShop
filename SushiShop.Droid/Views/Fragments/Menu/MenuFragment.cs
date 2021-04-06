e037ad0fdc8d698cc043c941c3e6c816e2d3231a
Alex Budiakov <>
2021-04-06 23:08:38 +0300

    wip

--- SushiShop.Droid/Views/Fragments/Menu/MenuFragment.cs
+++ SushiShop.Droid/Views/Fragments/Menu/MenuFragment.cs
@@ -1,4 +1,8 @@
-﻿using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.Fragments;
+﻿using Android.OS;
+using Android.Views;
+using Android.Widget;
+using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.Fragments;
+using MvvmCross.DroidX.RecyclerView;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using SushiShop.Core.ViewModels;
 using SushiShop.Core.ViewModels.Menu;
@@ -11,9 +15,28 @@ namespace SushiShop.Droid.Views.Fragments.Menu
         ActivityHostViewModelType = typeof(MainViewModel))]
     public class MenuFragment : BaseFragment<MenuViewModel>
     {
+        private MvxRecyclerView _listRecyclerView;
+        private MvxRecyclerView _gridRecyclerView;
+        private TextView _toolbarTitleTextView;
+
         public MenuFragment()
             : base(Resource.Layout.fragment_menu)
         {
+        }
+
+        protected override void InitializeViewPoroperties(View view, Bundle savedInstanceState)
+        {
+            base.InitializeViewPoroperties(view, savedInstanceState);
+
+            _listRecyclerView = View.FindViewById<MvxRecyclerView>(Resource.Id.list_recycler_view);
+            _gridRecyclerView = View.FindViewById<MvxRecyclerView>(Resource.Id.grid_recycler_view);
+            _toolbarTitleTextView = View.FindViewById<TextView>(Resource.Id.toolbar_title_text_view);
+            _listRecyclerView = View.FindViewById<MvxRecyclerView>(Resource.Id.list_recycler_view);
+        }
+
+        protected override void Bind()
+        {
+            base.Bind();
         }
     }
 }
