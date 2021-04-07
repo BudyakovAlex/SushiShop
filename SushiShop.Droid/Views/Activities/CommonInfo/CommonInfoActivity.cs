9a08d654e6a10f8a0e1e8c5b12d6a28752d94784
Alex Budiakov <>
2021-04-08 00:00:05 +0300

    wip

---
+++ SushiShop.Droid/Views/Activities/CommonInfo/CommonInfoActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Webkit;
+using AndroidX.AppCompat.Widget;
+using MvvmCross.Platforms.Android.Binding;
+using SushiShop.Core.ViewModels.Common;
+using SushiShop.Droid.Views.Activities.Abstract;
+using System;
+
+namespace SushiShop.Droid.Views.Activities.CommonInfo
+{
+    [Activity]
+    public class CommonInfoActivity : BaseActivity<CommonInfoViewModel>
+    {
+        private WebView webView;
+        private Toolbar toolbar;
+
+        public CommonInfoActivity() : base(Resource.Layout.activity_select_city)
+        {
+        }
+
+        protected override void InitializeViewPoroperties()
+        {
+            base.InitializeViewPoroperties();
+
+            webView = FindViewById<WebView>(Resource.Id.web_view);
+
+            toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+        }
+
+        protected override void Bind()
+        {
+            base.Bind();
+
+            using var bindingSet = CreateBindingSet();
+
+            bindingSet.Bind(this).For(v => v.Title).To(v => v.Title);
+            bindingSet.Bind(webView).For(v => v.BindWebViewHtml()).To(v => v.Content);
+        }
+    }
+}
