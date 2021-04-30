0e88f903e1826fab990a7ad35ec535d0f7ca7d42
Alex Budiakov <>
2021-04-30 22:45:11 +0300

    wip

--- SushiShop.Droid/Views/ViewHolders/Promotions/PromotionItemViewHolder.cs
+++ SushiShop.Droid/Views/ViewHolders/Promotions/PromotionItemViewHolder.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Android.Views;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using SushiShop.Core.ViewModels.Promotions.Items;
+using SushiShop.Droid.Views.ViewHolders.Abstract;
+
 namespace SushiShop.Droid.Views.ViewHolders.Promotions
 {
-    public class PromotionItemViewHolder
+    public class PromotionItemViewHolder : CardViewHolder<PromotionItemViewModel>
     {
-        public PromotionItemViewHolder()
+        public PromotionItemViewHolder(View view, IMvxAndroidBindingContext context) : base(view, context)
         {
         }
     }
