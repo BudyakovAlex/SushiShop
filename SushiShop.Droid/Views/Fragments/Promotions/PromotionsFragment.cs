7d0c075e4f2b31b7947fe7a0e5e7026a3e7e8771
Alex Budiakov <>
2021-04-30 22:44:28 +0300

    wip

--- SushiShop.Droid/Views/Fragments/Promotions/PromotionsFragment.cs
+++ SushiShop.Droid/Views/Fragments/Promotions/PromotionsFragment.cs
@@ -1,7 +1,19 @@
-﻿using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.Fragments;
+﻿using Android.OS;
+using Android.Views;
+using AndroidX.RecyclerView.Widget;
+using BuildApps.Core.Mobile.MvvmCross.UIKit.Adapter.TemplateSelectors;
+using BuildApps.Core.Mobile.MvvmCross.UIKit.Adapters;
+using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.Fragments;
+using MvvmCross.DroidX;
+using MvvmCross.DroidX.RecyclerView;
+using MvvmCross.Platforms.Android.Binding;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using SushiShop.Core.ViewModels;
 using SushiShop.Core.ViewModels.Promotions;
+using SushiShop.Core.ViewModels.Promotions.Items;
+using SushiShop.Droid.Views.LayoutManagers;
+using SushiShop.Droid.Views.ViewHolders.Promotions;
 
 namespace SushiShop.Droid.Views.Fragments.Promotions
 {
@@ -11,9 +23,68 @@ namespace SushiShop.Droid.Views.Fragments.Promotions
         ActivityHostViewModelType = typeof(MainViewModel))]
     public class PromotionsFragment : BaseFragment<PromotionsViewModel>
     {
+        private MvxRecyclerView recyclerView;
+        private MvxSwipeRefreshLayout swipeRefreshLayout;
+        private View loadingOverlayView;
+
         public PromotionsFragment()
             : base(Resource.Layout.fragment_promotions)
         {
+        }
+
+        protected override void InitializeViewPoroperties(View view, Bundle savedInstanceState)
+        {
+            base.InitializeViewPoroperties(view, savedInstanceState);
+
+            loadingOverlayView = View.FindViewById<View>(Resource.Id.loading_overlay_view);
+
+            swipeRefreshLayout = View.FindViewById<MvxSwipeRefreshLayout>(Resource.Id.list_refresh_layout);
+
+            InitializeListRecyclerView();
+        }
+
+        protected override void Bind()
+        {
+            base.Bind();
+
+            using var bindingSet = CreateBindingSet();
+
+            bindingSet.Bind(recyclerView).For(v => v.ItemsSource).To(vm => vm.Items);
+            bindingSet.Bind(loadingOverlayView).For(v => v.BindVisible()).To(vm => vm.IsBusy);
+            bindingSet.Bind(swipeRefreshLayout).For(v => v.Refreshing).To(vm => vm.IsRefreshing);
+            bindingSet.Bind(swipeRefreshLayout).For(v => v.RefreshCommand).To(vm => vm.RefreshDataCommand);
+        }
+
+        private void InitializeListRecyclerView()
+        {
+            recyclerView = View.FindViewById<MvxRecyclerView>(Resource.Id.recycler_view);
+            var adapter = new RecycleViewBindableAdapter((IMvxAndroidBindingContext)BindingContext);
+            recyclerView.Adapter = adapter;
+
+            var gridLayoutManager = new GridLayoutManager(Context, 2);
+            gridLayoutManager.SetSpanSizeLookup(new DelegateGridLayoutManagerSpanSizeLookup(GetRecyclerViewItemsSpan));
+
+            recyclerView.SetLayoutManager(gridLayoutManager);
+
+            recyclerView.ItemTemplateSelector = new TemplateSelector()
+                .AddElement<PromotionItemViewModel, PromotionItemViewHolder>(Resource.Layout.item_promotion);
+        }
+
+        private int GetRecyclerViewItemsSpan(int position)
+        {
+            if (position == 0)
+            {
+                return 2;
+            }
+
+            if (position % 3 == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
         }
     }
 }
---
+++ SushiShop.Droid/Views/ViewHolders/Promotions/PromotionItemViewHolder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SushiShop.Droid.Views.ViewHolders.Promotions
+{
+    public class PromotionItemViewHolder
+    {
+        public PromotionItemViewHolder()
+        {
+        }
+    }
+}
