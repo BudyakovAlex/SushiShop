5e3cf0f7ebcbc2ee8a62117738dd3f13bc9517ce
Alex Budiakov <>
2021-05-17 23:47:12 +0300

    wip

---
+++ SushiShop.Droid/Views/Fragments/Feedback/FeedbackFragment.cs
@@ -0,0 +1,21 @@
+﻿using Android.Views;
+using AndroidX.AppCompat.Widget;
+using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.Fragments;
+using MvvmCross.DroidX.RecyclerView;
+using SushiShop.Core.ViewModels.Feedback;
+using SushiShop.Droid.Presenter.Attributes;
+
+namespace SushiShop.Droid.Views.Fragments.Feedback
+{
+    [NestedFragmentPresentation(FragmentContentId = Resource.Id.container_view)]
+    public class FeedbackFragment : BaseFragment<FeedbackViewModel>
+    {
+        private Toolbar toolbar;
+        private View loadingOverlayView;
+        private MvxRecyclerView recyclerView;
+
+        public FeedbackFragment() : base(Resource.Layout.fragment_feedback)
+        {
+        }
+    }
+}
--- SushiShop.Droid/Views/Fragments/Orders/OrderCompositionFragment.cs
+++ SushiShop.Droid/Views/Fragments/Orders/OrderCompositionFragment.cs
@@ -1,14 +1,57 @@
-﻿using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.Fragments;
+﻿using Android.OS;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using BuildApps.Core.Mobile.MvvmCross.UIKit.Adapter.TemplateSelectors;
+using BuildApps.Core.Mobile.MvvmCross.UIKit.Adapters;
+using BuildApps.Core.Mobile.MvvmCross.UIKit.Views.Fragments;
+using MvvmCross.DroidX.RecyclerView;
+using MvvmCross.Platforms.Android.Binding;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
 using SushiShop.Core.ViewModels.Orders;
+using SushiShop.Core.ViewModels.Orders.Items;
 using SushiShop.Droid.Presenter.Attributes;
+using SushiShop.Droid.Views.ViewHolders.Orders;
 
 namespace SushiShop.Droid.Views.Fragments.Orders
 {
     [NestedFragmentPresentation(FragmentContentId = Resource.Id.container_view)]
     public class OrderCompositionFragment : BaseFragment<OrderCompositionViewModel>
     {
-        public OrderCompositionFragment() : base(resourceId)
+        private Toolbar toolbar;
+        private View loadingOverlayView;
+        private MvxRecyclerView recyclerView;
+
+        public OrderCompositionFragment() : base(Resource.Layout.fragment_order_composition)
         {
+        }
+
+        protected override void InitializeViewPoroperties(View view, Bundle savedInstanceState)
+        {
+            base.InitializeViewPoroperties(view, savedInstanceState);
+
+            toolbar = view.FindViewById<Toolbar>(Resource.Id.toolbar);
+            loadingOverlayView = view.FindViewById<View>(Resource.Id.loading_overlay_view);
+
+            InitializeRecyclerView(view);
+        }
+
+        protected override void Bind()
+        {
+            base.Bind();
+
+            using var bindingSet = CreateBindingSet();
+
+            bindingSet.Bind(loadingOverlayView).For(v => v.BindVisible()).To(v => v.IsBusy);
+            bindingSet.Bind(toolbar).For(v => v.Title).To(v => v.Title);
+            bindingSet.Bind(recyclerView).For(v => v.ItemsSource).To(vm => vm.Items);
+        }
+
+        private void InitializeRecyclerView(View view)
+        {
+            recyclerView = view.FindViewById<MvxRecyclerView>(Resource.Id.recycler_view);
+            recyclerView.Adapter = new RecycleViewBindableAdapter((IMvxAndroidBindingContext)BindingContext);
+            recyclerView.ItemTemplateSelector = new TemplateSelector()
+                .AddElement<OrderProductItemViewModel, OrderProductItemViewHolder>(Resource.Layout.item_order_product);
         }
     }
 }
